bee84e84ed14b683ca443ecd41e71708f305ae4f
Michael <m.squillacioti16@gmail.com>
2022-11-07 14:48:48 -0500

    moved index.css back into src folder, and moved the styling links that were in index.html into index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import '../public/index.css';
 import App from './components/App';
+
+<link font-family: "Montserrat", sans-serif;
+href="https://fonts.googleapis.com/css?family=McLaren|Montserrat&display=swap"
+rel="stylesheet" />
+<link rel="stylesheet" href="index.css" />
+<link id="external-css" rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Montserrat&amp;display=swap" media="all">
+
+import 'index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
