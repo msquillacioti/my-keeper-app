f998344b4fe1c11fe4e9e0b10fbe8b459be3c863
Michael <m.squillacioti16@gmail.com>
2022-11-07 14:28:01 -0500

    replaced all material-mui links with the updated mui/material links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import HighlightIcon from "@material-ui/icons/Highlight";
+// import HighlightIcon from "@material-ui/icons/Highlight";
 
 function Header() {
   return (
     <header>
       <h1>
-        <HighlightIcon />
+        // <HighlightIcon />
         Keeper
       </h1>
     </header>
