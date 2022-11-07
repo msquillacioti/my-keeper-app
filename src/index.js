6dbba33ec2871f7aeaac532320e7d732d70bb5c3
Michael <m.squillacioti16@gmail.com>
2022-11-07 14:53:19 -0500

    changed googleapis links to an import statement within index.js, before importing index.css

--- src/index.js
+++ src/index.js
@@ -2,11 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
 
-<link font-family: "Montserrat", sans-serif;
-href="https://fonts.googleapis.com/css?family=McLaren|Montserrat&display=swap"
-rel="stylesheet" />
-<link rel="stylesheet" href="index.css" />
-<link id="external-css" rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Montserrat&amp;display=swap" media="all">
+// Attempt to reference fonts from googleapis.
+import url('https://fonts.googleapis.com/css?family=McLaren|Montserrat&display=swap');
 
 import 'index.css';
 
